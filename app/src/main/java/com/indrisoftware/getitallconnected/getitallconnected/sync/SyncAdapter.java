46e23d99334bd3a5174af8de2826b08b909d13df
JanHerman <jhvds@xs4all.nl>
2018-10-03 22:03:36 +0200

    initial version

--- app/src/main/java/com/indrisoftware/getitallconnected/getitallconnected/sync/SyncAdapter.java
+++ app/src/main/java/com/indrisoftware/getitallconnected/getitallconnected/sync/SyncAdapter.java
@@ -5,16 +5,81 @@ import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.Context;
 import android.content.SyncResult;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 class SyncAdapter extends AbstractThreadedSyncAdapter {
-    
+    public final String LOG_TAG = SyncAdapter.class.getSimpleName();
+
     public SyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
     }
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+        Log.d(LOG_TAG, "Starting sync");
 
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String jsonStr = null;
+
+        final String CIAC_BASE_URL =
+                "http://api.openweathermap.org/data/2.5/forecast/daily?";
+
+        Uri builtUri = Uri.parse(CIAC_BASE_URL).buildUpon().build();
+
+        try {
+            URL url = new URL(builtUri.toString());
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return;
+            }
+            jsonStr = buffer.toString();
+            getDataFromJson(jsonStr);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getDataFromJson(String jsonStr) {
     }
 }
